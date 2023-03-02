ec4334999bff92e8c6d835d57757edabfcb9783b
Ayoub <ayoub.fiadh@gmail.com>
2023-03-01 19:19:52 -0500

    add v-app-bar

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template lang="pug">
 v-app
+  v-app-bar(:elevation="2" rounded density="compact")
+    v-toolbar-title Shopping
+    v-spacer
+    v-btn(icon)
+      v-icon mdi-cart-outline
+    v-btn(icon)
+      v-icon mdi-account-circle
   Nuxt
   Home
   Footer
